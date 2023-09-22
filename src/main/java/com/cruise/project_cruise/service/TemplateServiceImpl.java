0c842df6cdca32b0625b3cee9b83b8fd12362d29
eunzzzzz1 <eunzzzzz1@kakao.com>
2023-09-22 12:10:08 +0900

    230922 [은지] mybatis 환경세팅

--- src/main/java/com/cruise/project_cruise/service/TemplateServiceImpl.java
+++ src/main/java/com/cruise/project_cruise/service/TemplateServiceImpl.java
@@ -4,7 +4,9 @@ import com.cruise.project_cruise.dto.TemplateDTO;
 import com.cruise.project_cruise.mapper.TemplateMapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class TemplateServiceImpl implements TemplateService {
 
     @Autowired
