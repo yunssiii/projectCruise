047848da1f54d4a7b5105e47bd3a246d0f24ae80
WIN-R9215F45F4N\itwill <aldus7261@gmail.com>
2023-10-05 16:25:47 +0900

    10/05 [이미연] 크루 모임 게시판, 댓글

---
+++ src/main/java/com/cruise/project_cruise/mapper/CrewCommentMapper.java
@@ -0,0 +1,19 @@
+package com.cruise.project_cruise.mapper;
+
+
+import com.cruise.project_cruise.dto.CrewCommentDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface CrewCommentMapper {
+
+	public int maxNum() throws Exception;
+	public void insertData(CrewCommentDTO dto) throws Exception;
+	public int getDataCount(int board_num) throws Exception;
+	public List<CrewCommentDTO> getLists(int board_num) throws Exception;
+	public void updateData(CrewCommentDTO dto) throws Exception;
+	public void deleteData(int comment_num) throws Exception;
+}
+
---
+++ src/main/java/com/cruise/project_cruise/service/CrewBoardService.java
@@ -0,0 +1,22 @@
+package com.cruise.project_cruise.service;
+
+
+import com.cruise.project_cruise.dto.CrewBoardDTO;
+
+import java.util.List;
+
+
+public interface CrewBoardService {
+
+	public int maxNum() throws Exception;
+	public void insertData(CrewBoardDTO dto) throws Exception;
+	public int getDataCount(String searchKey, String searchValue,
+							int crew_num) throws Exception;
+	public List<CrewBoardDTO> getLists(int start, int end,
+									   String searchKey, String searchValue,
+									   int crew_num) throws Exception;
+	public CrewBoardDTO getReadData(int num) throws Exception;
+	public void updateHitCount(int num) throws Exception;
+	public void updateData(CrewBoardDTO dto) throws Exception;
+	public void deleteData(int num) throws Exception;
+}
