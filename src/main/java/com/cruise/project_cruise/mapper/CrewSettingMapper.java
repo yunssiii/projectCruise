38a7755c0b93ee8aa96f174090c3a0ff36ef0e96
eunzzzzz1 <eunzzzzz1@kakao.com>
2023-10-11 14:33:40 +0900

    231011 [은지] 크루상세페이지 백엔드 진행중

---
+++ src/main/java/com/cruise/project_cruise/mapper/CrewSettingMapper.java
@@ -0,0 +1,11 @@
+package com.cruise.project_cruise.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface CrewSettingMapper {
+
+//    public int selectMethod() throws Exception;
+//    public void insertMethod(TemplateDTO dto) throws Exception;
+
+}
