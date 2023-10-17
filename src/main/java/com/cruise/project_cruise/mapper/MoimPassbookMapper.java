665247fcd72b98d511a9523d3c54eaf5f797d268
WIN-R9215F45F4N\itwill <aldus7261@gmail.com>
2023-10-17 16:21:24 +0900

    10/17 [이미연] - 크루 모임 게시판 crewNum 수정 - 모임통장 전환 페이지

---
+++ src/main/java/com/cruise/project_cruise/mapper/MoimPassbookMapper.java
@@ -0,0 +1,20 @@
+package com.cruise.project_cruise.mapper;
+
+import com.cruise.project_cruise.dto.CrewDTO;
+import com.cruise.project_cruise.dto.CrewMemberDTO;
+import com.cruise.project_cruise.dto.MyAccountDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+
+@Mapper
+public interface MoimPassbookMapper {
+
+	public int maxCrewNum() throws Exception;
+	public void insertCrew(CrewDTO dto) throws Exception;
+	public void insertCrewMember(CrewMemberDTO dto) throws Exception;
+	public List<MyAccountDTO> getMyAccount(String email) throws Exception;
+
+}
+
