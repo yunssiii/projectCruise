2d76dfe476b2109eeeeb7a95b2d63610ea88c641
eunzzzzz1 <eunzzzzz1@kakao.com>
2023-10-20 16:08:51 +0900

    231020 [은지] setting 선원관리 - 선원 강퇴기능 구현

--- src/main/java/com/cruise/project_cruise/service/CrewSettingService.java
+++ src/main/java/com/cruise/project_cruise/service/CrewSettingService.java
@@ -1,6 +1,7 @@
 package com.cruise.project_cruise.service;
 
 import com.cruise.project_cruise.dto.CrewDTO;
+import com.cruise.project_cruise.dto.CrewMemberDTO;
 import com.cruise.project_cruise.dto.ScheduleDTO;
 import com.cruise.project_cruise.dto.UserDTO;
 import org.apache.ibatis.annotations.Mapper;
@@ -8,13 +9,23 @@ import org.apache.ibatis.annotations.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Map;
 
 public interface CrewSettingService {
+    // red 크루 관리
+    // 크루 정보수정
     public List<ScheduleDTO> getCrewScheList(@Param("crew_num") int crewNum) throws Exception;
-    public UserDTO getCrewCaptain(@Param("captainEmail") String captainEmail) throws Exception;
+    public UserDTO getUser(@Param("email") String userEmail) throws Exception;
+
+    // 크루 일정관리
     public void updateCrewInfo(CrewDTO crewDTO) throws Exception;
     public void insertCrewSche(ScheduleDTO scheduleDTO) throws Exception;
     public void updateCrewSche(ScheduleDTO scheduleDTO) throws Exception;
     public void deleteCrewSche(@Param("sche_num") int scheNum) throws Exception;
     public int getScheMaxNum() throws Exception;
+
+
+    // red 선원 관리
+    public List<Map<String,String>> getCrewMemberList(@Param("crew_num") int crewNum) throws Exception;
+    public void deleteMember(@Param("email") String banEmail,@Param("crew_num") int crewNum) throws Exception;
 }
