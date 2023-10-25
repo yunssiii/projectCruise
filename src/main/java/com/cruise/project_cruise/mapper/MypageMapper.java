63cfb28ce2e76cde4fc534e71e4b58a216d3029f
황윤하 <hchdbsgk@naver.com>
2023-10-25 13:39:28 +0900

    231025 윤하 1차 알림 제외 완료

--- src/main/java/com/cruise/project_cruise/mapper/MypageMapper.java
+++ src/main/java/com/cruise/project_cruise/mapper/MypageMapper.java
@@ -40,4 +40,5 @@ public interface MypageMapper {
     public List<ScheduleDTO> getSchedule(String email) throws Exception;
     public List<ScheduleDTO> getOneSchedule(@Param("email") String email,@Param("sche_start") String scheStart) throws Exception;
     public String getScheCrewName(@Param("email") String email,@Param("crew_num")int crew_num) throws Exception;
+    public void deleteUser(String email) throws Exception;
 }
--- src/main/resources/static/js/mypage/mypage_userOut.js
+++ src/main/resources/static/js/mypage/mypage_userOut.js
@@ -10,8 +10,9 @@ var nDivs = document.getElementsByClassName("n-div");
 var okBtn1 = document.getElementsByClassName("yes");
 var okBtn2 = document.getElementsByClassName("no");
 
-// "회원 탈퇴" 버튼 js
+// 회원 탈퇴 js
 
+//'화원 탈퇴' 누르면 모달
 withdrawalBtns[0].onclick = function() {
 
     userOutDiv[0].style.display = "block";
@@ -19,34 +20,67 @@ withdrawalBtns[0].onclick = function() {
 
 }
 
-yBtn[0].onclick = function() {
+// '예' 누르면 탈퇴 처리
+function deleteUser(email){
 
-    //db에 회원정보 delete 
+    $.ajax({
+        type : "POST",
+        url : "/mypage/deleteUser",
+        data : {
+        email : email, // 로그인한 이메일
+        },
+        success : function(result){
+            console.log("탈퇴성공..!");
 
-    userOutDiv[0].style.display = "none";
-    yDivs[0].style.display = "block";
+            //모달 열기
+            $('.userOutmodal-div').css('display','none');
+            $('.y-div').css('display','block');
+
+        },
+        error:function(xhr,status,error) {
+            // 모임통장 계좌번호와 같으면 에러로 넘어옴
+            console.log("탈퇴에러..!" + error);
+
+            //모달 열기
+            $('.userOutmodal-div').css('display','none');
+            $('.n-div').css('display','block');
+
+        }
+    });
+};
+
+// '아니오' 누르면 모달 닫기
+function closeModal() {
+
+    $('.userOutmodal-div').css('display','none');
+
+    $('body').css('overflow', 'auto');
+    $('body').css('overflow-x', 'hidden');
+
 }
 
-nBtn[0].onclick = function() {
+// 탈퇴완료의 '확인' 함수
+function okMain() {
 
-    userOutDiv[0].style.display = "none";
-    nDivs[0].style.display = "block";
+    $('.y-div').css('display','none');
+
+    $('body').css('overflow', 'auto');
+    $('body').css('overflow-x', 'hidden');
+
+    //location.replace("/logout"); //전 페이지로 돌아갈 수 없음
+
+    localStorage.removeItem('accessToken');
+    window.location.href = '/logout';
 }
 
-okBtn1[0].onclick = function() {
+// 선장의 '확인' 함수
+function okMypage() {
 
-    // 메인페이지 이동
+    $('.n-div').css('display','none');
 
-    yDivs[0].style.display = "none";
-    document.body.style.overflow = "auto";
-    document.body.style.overflowX = "hidden";
+    $('body').css('overflow', 'auto');
+    $('body').css('overflow-x', 'hidden');
 
+    location.reload();
 }
 
-okBtn2[0].onclick = function() {
-
-    nDivs[0].style.display = "none";
-    document.body.style.overflow = "auto";
-    document.body.style.overflowX = "hidden";
-
-}
