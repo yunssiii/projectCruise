08e05b270965ae5d7b703ce6270e2b76bec800e4
eunzzzzz1 <eunzzzzz1@kakao.com>
2023-10-29 10:41:55 +0900

    231028 [은지] 스케줄링 세팅 및 컨트롤러 내 테스트

--- src/main/java/com/cruise/project_cruise/quartz/config/QuartzService.java
+++ src/main/java/com/cruise/project_cruise/quartz/config/QuartzService.java
@@ -25,6 +25,7 @@ public class QuartzService {
         try {
             // 스케줄러를 초기화하기
             scheduler.clear(); // DB도 초기화된다. 모든 예약된 작업과 트리거가 삭제된다.
+            // 최종적으론 이거 빼주기!
 
             // 만들어뒀던 리스너들을 등록해준다.
             scheduler.getListenerManager().addJobListener(new QuartzJobListner());
@@ -41,28 +42,13 @@ public class QuartzService {
                     이렇게 해줬었음.
              */
 
-            Map<String, Object> paramsMap = new HashMap<>();
-            paramsMap.put("executeCount",1);
-            // 앞서 실행횟수를 체크하는 변수로 설정해줬던 executeCount 를 1로 설정해 담아주고,
-            paramsMap.put("crewNum",1); // 일단 임의로 넣기. 1번 크루 알림에 넣어보자
-            paramsMap.put("cAlertContent","Quartz 테스트");
-            paramsMap.put("cAlertAssort","테스트");
-
-            // 그리고 date 를 로그에 출력하도록 해주었으니까 그것도 넣어주자!
-            LocalDateTime now = LocalDateTime.now();
-            paramsMap.put("date",now);
-
-            //Job을 생성해 Scheduler에 등록하자
-            addJob(QuartzJob.class, "QuartzJob", "TestJobs","Quartz Job 입니다", paramsMap, "0/5 * * * * ?"); // 5초간격으로 실행한다는 뜻
-
-
         }catch (Exception e) {
             log.error("addJob error : {}", e);
         }
     }
 
     //Job 추가하는 메소드
-    public <T extends Job> void addJob(Class<? extends Job> job , String key, String group, String desc, Map<String,Object> paramsMap, String cron) throws SchedulerException, SchedulerException {
+    public <T extends Job> void addCronJob(Class<? extends Job> job , String key, String group, String desc, Map<String,Object> paramsMap, String cron) throws SchedulerException, SchedulerException {
         JobDetail jobDetail = buildJobDetail(job,group,key,desc,paramsMap);
         Trigger trigger = buildCronTrigger(cron); // cron으로 트리거 생성하기
         if(scheduler.checkExists(jobDetail.getKey())) scheduler.deleteJob(jobDetail.getKey());
