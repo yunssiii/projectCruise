071eeddadf3e8740845b75dc6358ce65fc8899c5
eunzzzzz1 <eunzzzzz1@kakao.com>
2023-10-31 10:30:35 +0900

    231031 [은지] 자바스크립트 오류 수정

--- src/main/resources/static/js/crewDetail/TransactionDetailView.js
+++ src/main/resources/static/js/crewDetail/TransactionDetailView.js
@@ -1,7 +1,89 @@
 
-
 var accountHistoryTbody = document.getElementById('accountHistoryTbody');
 var crewAccount = document.getElementById('crewAccount').value;
+var inquiryReq = $.ajax({
+    url: "/develop/openbank/using/search",
+    method: "POST",
+    data: {
+        searchType:0,
+        selectedAccount:''+crewAccount,
+        content:'',
+        startDate:'',
+        endDate:''
+    }
+})
+
+
+
+inquiryReq.done(function(result) {
+
+    // 날짜를 기준으로 정렬
+    result.sort(function(a, b) {
+        var dateA = new Date(a.date);
+        var dateB = new Date(b.date);
+        return dateB - dateA; // 최신 날짜를 기준으로 정렬
+    });
+
+    var html = "";
+    var stat = 0;
+
+    result.forEach(function (dto) {
+
+        console.log("stat : " + stat);
+        if(stat>=9) {
+            return;
+        }
+
+        var transferDateStr = dto.date;
+        var transferDate = new Date(transferDateStr);
+
+        var year = (transferDate.getFullYear()+'').slice(2,4);
+        var month = transferDate.getMonth()
+        if(month<10) {
+            month = '0' + month;
+        }
+        var date = transferDate.getDate();
+        if(date<10) {
+            date = '0' + date;
+        }
+
+        transferDateStr = year + '.' + month + '.' + date;
+        console.log("assort: " + dto.assort);
+        var assorts = "";
+
+        if((dto.assort).trim()==='I') {
+            assorts = "입금";
+        } else {
+            assorts = "출금";
+        }
+
+
+        html += "<tr>" +
+            "<td>" + transferDateStr + "</td>" +
+            "<td>" + assorts +"</td>" +
+            "<td>" + dto.content + "</td>";
+
+        if(assorts==="입금") {
+            html += "<td style='text-align: right'>" + dto.inMoney.toLocaleString('ko-KR') + "</td>" +
+                "<td></td>"
+        } else {
+            html += "<td></td>" +
+                "<td style='text-align: right'>" + dto.outMoney.toLocaleString('ko-KR') + "</td>";
+        }
+
+        html += "<td style='text-align: right'>" + dto.balance.toLocaleString('ko-KR') + "</td>" +
+            "</tr>";
+
+        stat++;
+    })
+
+    accountHistoryTbody.innerHTML = html;
+})
+
+/*
+var accountHistoryTbody = document.getElementById('accountHistoryTbody');
+var crewAccount = document.getElementById('crewAccount').value;
+
 var inquiryReq = $.ajax({
     url: "/develop/openbank/using/search",
     method: "POST",
@@ -18,7 +100,7 @@ inquiryReq.done(function(result) {
 
     var html = "";
     var stat = 0;
-
+    console.log(result);
     for (let i=result.length-1; i>=0; i--) {
         console.log("stat : " + stat);
         if(stat>=9) {
@@ -66,8 +148,7 @@ inquiryReq.done(function(result) {
 
         stat++;
     }
-
+    console.log(html);
     accountHistoryTbody.innerHTML = html;
 })
-
-
+*/
