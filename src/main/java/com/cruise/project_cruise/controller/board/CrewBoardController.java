5709cdbe732ec7b83291e5fc6b15b1696077830d
eunzzzzz1 <eunzzzzz1@kakao.com>
2024-02-10 06:13:39 +0900

    240210 [은지/수정] CrewBoardController.java / created_ok() 공지 crewAlert 추가 시 내용 수정
    공지 등록 시 추가되는 CrewAlert의 내용이 "[공지]... " 로 등록되는 것이 효용성이 없다고 판단, 공지의 제목이 일부 보일 수 있도록 수정함

--- src/main/java/com/cruise/project_cruise/controller/board/CrewBoardController.java
+++ src/main/java/com/cruise/project_cruise/controller/board/CrewBoardController.java
@@ -141,7 +141,11 @@ public class CrewBoardController {
 
             // crew_alert 테이블에 insert
             // 은지 : 공지 알림 형식 수정할게요~!
-            String articleSubjectSub = dto.getBoard_subject().substring(0, 4) + "...";
+
+            String articleSubjectSub = dto.getBoard_subject().substring(4).trim();
+			if(articleSubjectSub.length()>4) {
+				articleSubjectSub = articleSubjectSub.substring(0, 3) + "...";
+			}
             String crewAlertContent = "\"" + articleSubjectSub + "\" 가 등록되었습니다.";
             crewAlertService.insertCrewAlert(crewAlertService.cAlertMaxNum() + 1, dto.getCrew_num(),
                     "공지", crewAlertContent, today);
